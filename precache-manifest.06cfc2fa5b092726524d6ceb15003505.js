0bd8174f65100c08cd169a08bbe22f7ae96bb7a0
Kien Do <kien.do@liferay.com>
2021-07-13 16:20:45 -0700

    Add build directory to deployment branch

---
+++ precache-manifest.06cfc2fa5b092726524d6ceb15003505.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3bb07ac5c2ca729a2339",
+    "url": "/language-key-creator/static/js/runtime~main.3bb07ac5.js"
+  },
+  {
+    "revision": "4e45d479d617dac02742",
+    "url": "/language-key-creator/static/js/main.4e45d479.chunk.js"
+  },
+  {
+    "revision": "69c5b163431ecf47ae4a",
+    "url": "/language-key-creator/static/js/1.69c5b163.chunk.js"
+  },
+  {
+    "revision": "4e45d479d617dac02742",
+    "url": "/language-key-creator/static/css/main.2a4f48da.chunk.css"
+  },
+  {
+    "revision": "2b3d6eea98f84c5252558e2cdfaf4a0d",
+    "url": "/language-key-creator/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/language-key-creator/precache-manifest.06cfc2fa5b092726524d6ceb15003505.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/language-key-creator/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
